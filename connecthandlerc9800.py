1201b9b64ee03c26d4f37888ebbcd2aa4cf8cd99
adam-wifitech-blog <150694572+adam-wifitech-blog@users.noreply.github.com>
2024-01-05 15:26:08 +0100

    Update connecthandlerc9800.py
    test_tcp_connection() - checks whether it's possible to establish a connection to a given host using SSH protocol by default.
    check_access() - verifies whether the provided credentials allow logging into the respective device.

--- connecthandlerc9800.py
+++ connecthandlerc9800.py
@@ -1,25 +1,48 @@
 #!/usr/bin/env python3
 
-from netmiko import ConnectHandler
+from netmiko import ConnectHandler, NetmikoAuthenticationException
 import time
+import socket
 
-def connecthandlerc9800(device, send_command):
-    # Record the start time of the operation.
-    start_time = time.time()
+def connecthandlerc9800(device,send_command):
     # Establish a connection to the network device using Netmiko.
     connect = ConnectHandler(**device)
     # Send the command to the device and store the output.
     send_command_output = connect.send_command(send_command, read_timeout=320)
     # Disconnect from the device.
     connect.disconnect()
-    # Record the end time of the operation.
-    end_time = time.time()
-    # Calculate the total time taken for the operation.
-    total_time = end_time - start_time
-    if __name__ == '__main__':
-        print(f'\n Total time script execution: {round(total_time, 3)} \n')
     # Return the output of the sent command.
     return send_command_output
+
+"""Checking the TCP connection to the host."""
+def test_tcp_connection(host,port=22,timeout=3):
+    try:
+        with socket.create_connection((host,port),timeout):
+            return True
+    except:
+        return False
+
+def check_access(ip_address,username,password,secret, device_type='cisco_ios', port=22):
+    # Connection device details
+    device = {
+        'host': ip_address,
+        'device_type': device_type,
+        'username': username,
+        'password': password,
+        'port': port,
+        'secret': secret,
+    }
+
+    #Check if authentication using credentials occurs successfully or not
+
+    try:
+        connection = ConnectHandler(**device)
+        connection.disconnect()
+        message = "Auth Success"
+        return message
+    except NetmikoAuthenticationException:
+        message = "Auth Failed"
+        return message
 
 # Main execution block (only runs if this script is the main program).
 if __name__ == '__main__':
@@ -32,6 +55,13 @@ if __name__ == '__main__':
         'port': 22,
         'secret': input("Enter your secret password: "),
     }
+    # Record the start time of the operation.
+    start_time = time.time()
     # Execute the connect handler to send a command to the device and receive output.
     send_command_output = connecthandlerc9800(device, input('Provide the command to send: '))
+    # Record the end time of the operation.
+    end_time = time.time()
+    # Calculate the total time taken for the operation.
+    total_time = end_time - start_time
     print(f'Object created: \n \n {send_command_output}')
+    print(f'\n Total time script execution: {round(total_time, 3)} \n')
